9511ad93c3839778fad77e7e6fb605ded34e5e5b
MF <mfregwork21@gmail.com>
2025-03-18 15:36:16 -0700

    Automatically refreshes page using router

--- src/app/admin/products/_components/ProductActions.tsx
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -3,16 +3,19 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import { useTransition } from "react"
 import { deleteProduct, toggleProductAvailability } from "../../actions/products"
+import { useRouter } from "next/navigation"
 
 export function ActiveToggleDropdownItem({id, isAvailableForPurchase}:{
     id:string 
     isAvailableForPurchase:boolean
 }){
     const [isPending, startTransition] = useTransition()
+    const router = useRouter()
     return (
         <DropdownMenuItem disabled={isPending} onClick={()=> {
             startTransition(async()=> {
                 await toggleProductAvailability(id, !isAvailableForPurchase)
+                router.refresh()
             })
         }}>
             {isAvailableForPurchase ?"Deactivate":"Activate"}
@@ -22,10 +25,12 @@ export function ActiveToggleDropdownItem({id, isAvailableForPurchase}:{
 
 export function DeleteDropdownItem({id, disabled}: {id:string, disabled:boolean}){
     const [isPending, startTransition] = useTransition()
+    const router = useRouter()
     return (
         <DropdownMenuItem variant="destructive" disabled={disabled || isPending} onClick={()=> {
             startTransition(async()=> {
                 await deleteProduct(id)
+                router.refresh()
             })
         }}>
             Delete
