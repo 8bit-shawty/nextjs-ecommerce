0ff1ccd3a76ff6e7ff3c1a7202a8ce60c726f28c
MF <mfregwork21@gmail.com>
2025-03-17 20:30:10 -0700

    Created loader

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,78 @@
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import prisma from "@/db/prisma";
+import { formatNumber,formatCurrency } from "@/lib/formatters";
 
-export default function AdminDashboard(){
+async function getSalesData(){
+    const data = await prisma.order.aggregate({
+        _sum:{pricePaidInCents:true},
+        _count:true
+    })
+
+    await wait(2000)
+
+    return {
+        amount:(data._sum.pricePaidInCents || 0) / 100,
+        numberOfSales:data._count
+    }
+}
+
+function wait(duration){
+    return new Promise(resolve => setTimeout(resolve,duration))
+}
+
+async function getUserData(){
+    const [userCount, orderData] = await Promise.all([
+        prisma.user.count(),
+        prisma.order.aggregate({
+            _sum:{pricePaidInCents:true},
+        })
+    ])
+
+    return {
+        userCount,
+        averageValuePerUser:userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / userCount / 100
+    }
+}
+
+async function getProductData(){
+    const [activeCount, inactiveCount] = await Promise.all([
+        prisma.product.count({where:{isAvailableForPurchase:true}}),
+        prisma.product.count({where:{isAvailableForPurchase:false}})
+    ])
+
+    return {activeCount,inactiveCount}
+}
+
+export default async function AdminDashboard(){
+    const[salesData, userData, productData] = await Promise.all([
+        getSalesData(),
+        getUserData(),
+        getProductData()
+    ])
+
     return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Card>
-            <CardHeader>Sales</CardHeader>
-            <CardDescription>Lorem ipsum dolor sit amet consectetur, adipisicing elit. !</CardDescription>
-            <CardContent>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius odio atque vel animi veniam at cum beatae tenetur qui! Tenetur?</p>
-            </CardContent>
-        </Card>
+        <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} body={formatCurrency(salesData.amount)}/>
+        <DashboardCard title="Customers" subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`} body={formatNumber(userData.userCount)}/>
+        <DashboardCard title="Customers" subtitle={`${formatNumber(productData.inactiveCount)} Inactive`} body={formatNumber(productData.activeCount)}/>
     </div>
+}
+
+type DashboardCardProps = {
+    title:string
+    subtitle:string
+    body:string
+}
+
+function DashboardCard ({title,subtitle,body}:DashboardCardProps){
+    return (
+    <Card>
+        <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{subtitle}</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>{body}</p>
+        </CardContent>
+    </Card>
+    )
 }
