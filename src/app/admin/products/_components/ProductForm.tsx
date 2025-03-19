d7c8a4c9331e05ad3b993542b6edc7ab93e7f97c
MF <mfregwork21@gmail.com>
2025-03-18 20:39:08 -0700

    Successfully updated product using UI

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,12 +6,13 @@ import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { formatCurrency } from "@/lib/formatters"
 import { useState } from "react"
-import { addProduct } from "../../actions/products"
+import { addProduct, updateProduct } from "../../actions/products"
 import { useFormState, useFormStatus } from "react-dom"
 import {Product} from "@prisma/client"
+import Image from "next/image"
 
 export function ProductForm({product}:{product?:Product | null}){
-    const[error,action] = useFormState(addProduct,{})
+    const[error,action] = useFormState(product == null ? addProduct : updateProduct.bind(null,product.id),{})
     const[priceInCents,setPriceInCents] = useState<number | undefined>(product?.priceInCents)
     return(
         <form action={action} className="space-y-8">
@@ -37,12 +38,18 @@ export function ProductForm({product}:{product?:Product | null}){
             <div className="space-y-2">
                 <Label htmlFor="file">File</Label>
                 <Input type="file" id="file" name="file" required={product == null}/>
+                {product != null && (
+                    <div className="text-muted-foreground">{product.filePath}</div>
+                )}
                 {error.file && <div className="text-destructive">{error.file}</div>}
             </div>
 
             <div className="space-y-2">
                 <Label htmlFor="image">Image</Label>
                 <Input type="file" id="image" name="image" required/>
+                {product != null && (
+                    <Image src={product.imagePath} height='400' width='400' alt="Product image"/>
+                )}
                 {error.image && <div className="text-destructive">{error.image}</div>}
             </div>
             <SubmitButton/>
