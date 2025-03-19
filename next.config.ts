e5106e0c5961b5eceebf2294233cf10ce6051b3e
MF <mfregwork21@gmail.com>
2025-03-19 14:13:38 -0700

    Successfully rendered products for user homepage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb", // Increase to desired limit
+    },
+  },
 };
 
 export default nextConfig;
