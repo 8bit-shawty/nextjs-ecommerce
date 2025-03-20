861a79b1b164c147e2866e38636ade1b1948abbc
MF <mfregwork21@gmail.com>
2025-03-20 16:28:11 -0700

    Created stripe form with successful payment intent

---
+++ src/app/actions/orders.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import prisma from "@/db/prisma"
+
+
+export async function userOrderExists(email: string, productId: string) {
+  return (
+    (await prisma.order.findFirst({
+      where: { user: { email }, productId },
+      select: { id: true },
+    })) != null
+  )
+}
